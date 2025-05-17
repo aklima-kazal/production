5f5020bf05ac4d72cdc36bd46b02b8336f93cb35
aklima-kazal <aklimakajal11@gmail.com>
2025-05-17 18:01:12 +0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
