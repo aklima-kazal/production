197d4052c1c360702c57be9cc8261831cbb231e6
aklima-kazal <aklimakajal11@gmail.com>
2025-05-21 01:08:55 +0600

    last

--- src/GlobalComponents/TopBar/TopBar.jsx
+++ src/GlobalComponents/TopBar/TopBar.jsx
@@ -12,22 +12,22 @@ const TopBar = () => {
   return (
     <>
       <Container>
-        <div className="block md:hidden ">
+        <div className="block md:hidden mb-[10px]">
           <img
             src="assets/img/Logo.svg"
             className="mx-auto xs:w-[80px] xs:h-[60px] md:max-w-[80px] lg:min-w-[180px] lg:h-[80px] "
             alt="image"
           />
         </div>
-        <div className="text-[#FFFFFF] relative h-[40px] mb-[40px] lg:mb-[50px] top-[-20px] md:top-[40px] items-center justify-center p-4 md:p-0">
-          <div className="lg:border-b lg:border-b-[rgba(152,154,169,0.6)] absolute z-50 md:w-full w-[120%] md:p-4 lg:p-0 flex items-center lg:justify-center md:justify-around xs:gap-x-[10px]  lg:gap-x-[105px] ">
-            <div className="flex items-center justify-center  xs:space-x-[3px] md:space-x-[20px]">
-              <FaFacebookF />
-              <IoLogoTwitter />
-              <FaInstagram />
-              <FaYoutube />
+        <div className="text-[#FFFFFF] relative h-[40px] mb-[40px] lg:mb-[50px] top-[-20px] md:top-[40px] items-center justify-center p-2 md:p-0">
+          <div className="lg:border-b lg:border-b-[rgba(152,154,169,0.6)] absolute z-50 md:w-full w-[105%] md:p-4 lg:p-0 flex items-center lg:justify-center md:justify-around xs:gap-x-[20px]  lg:gap-x-[105px] ">
+            <div className="flex items-center justify-center  xs:space-x-[4px] md:space-x-[20px]">
+              <FaFacebookF className="lg:w-[15px] lg:h-[20px] md:w-[12px] md:h-[18px] sm:w-[10px] sm:h-[16px] " />
+              <IoLogoTwitter className="lg:w-[20px] lg:h-[25px] md:w-[18px] md:h-[20px] sm:w-[10px] sm:h-[16px]" />
+              <FaInstagram className="lg:w-[20px] lg:h-[25px] md:w-[18px] md:h-[20px] sm:w-[10px] sm:h-[16px]" />
+              <FaYoutube className="lg:w-[25px] lg:h-[30px] md:w-[20px] md:h-[25px] sm:w-[10px] sm:h-[16px]" />
 
-              <IoLogoLinkedin />
+              <IoLogoLinkedin className="lg:w-[25px] lg:h-[30px] md:w-[20px] md:h-[25px] sm:w-[10px] sm:h-[16px]" />
             </div>
             <div className="hidden md:block">
               <img
@@ -38,12 +38,12 @@ const TopBar = () => {
             </div>
 
             <div className="flex space-x-[30px] lg:space-x-[60px] items-center justify-center ">
-              <div className="flex items-center gap-x-[10px] text-[12px]">
+              <div className="flex items-center gap-x-[10px] text-[12px] md:text-[14px] lg:text-[17px]">
                 <BsFillTelephoneFill className="" />
                 (+021) 345 678 910
               </div>
 
-              <div className="flex items-center gap-x-[10px] text-[12px]">
+              <div className="flex items-center gap-x-[10px] text-[12px] md:text-[14px] lg:text-[18px]">
                 <IoMail className=" lg:w-[30px] lg:h-[20px]" />
                 info@filmina.com
               </div>
